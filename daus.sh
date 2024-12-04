a6f5f8be8b4d0e8dd0056fcd545d0367e968ae61
BanSudoSu <abdulbasikal@gmail.com>
2024-12-04 16:59:57 +0700

    Add files via upload

--- daus.sh
+++ daus.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/expect
 
-# Spawn telnet session
 spawn telnet 192.168.234.132 30016
-
-# Set timeout untuk menunggu respon dari telnet
 set timeout 10
 
-# Login dengan username "admin" dan password kosong
-expect "Mikrotik Login: " { send "admin\r" }
-expect "Password: " { send "\r" }   # Kosongkan password
+# Login
+expect "Login: " { send "admin\r" }
+expect "Password: " { send "\r" }
 
-# Set password baru (apabila diminta) dan ulangi password baru
-expect "new password>" { send "123\r" }
-expect "repeat new password>" { send "123\r" }
+# Jika ada prompt untuk lisensi, jawab "n"
+expect {
+    "Do you want to see the software license? [Y/n]:" { send "n\r" }
+    "new password>" { send "123\r" }
+}
 
-# Setelah login, kirim perintah untuk menambahkan IP address
+# Mengubah password baru
+expect "new password>" { 
+    sleep 1
+    send "123\r"
+}         
+expect "repeat new password>" { 
+    sleep 1
+    send "123\r"
+}
+
+# Menambahkan IP Address
 expect ">" { send "/ip address add address=192.168.200.1/24 interface=ether2\r" }
 
-# Kirim perintah untuk menambahkan NAT rule
+# Menambahkan NAT Masquerade
 expect ">" { send "/ip firewall nat add chain=srcnat out-interface=ether2 action=masquerade\r" }
 
-# Kirim perintah untuk menambahkan route
-expect ">" { send "/ip route add dst-address=192.168.200.0/24 gateway=192.168.20.1\r" }
+# Menambahkan Route
+expect ">" { send "/ip route add dst-address=192.168.200.1/24 gateway=192.168.20.1\r" }
 
-# Keluar dari session
+# Keluar dari MikroTik
 expect ">" { send "quit\r" }
-
-# Tunggu hingga session selesai
 expect eof
